0ed97560148c895ea14af6b3411d6eabd046576d
DiAmina <amina.madi@etu.iut-tlse3.fr>
2023-11-27 09:15:15 +0100

    update fonction moyenneSalaire MAJ

--- js/script.js
+++ js/script.js
@@ -4,6 +4,26 @@ let request = $.ajax({
     url: "../data/survey_results.json"
 });
 
+EUROPE_COUNTRIES = {
+    0: "Germany",
+    1: "United Kingdom of Great Britain and Northern Ireland",
+    3: "France",
+    4: "Poland",
+    5: "Netherlands",
+    6: "Italy",
+    7: "Spain",
+    8: "Switzerland",
+    9: "Belgium",
+    10: "Portugal",
+    11: "Ireland",
+};
+
+AMERICA_COUNTRIES = {
+    0: "United States of America",
+    1: "Canada",
+};
+
+
 // Fonction pour obtenir les salaires moyens en fonction de l'expérience
 function getMeanSalaryByExp(data, continent, pays) {
     const meanSalaries = {};
@@ -11,12 +31,12 @@ function getMeanSalaryByExp(data, continent, pays) {
 
     // Filtrer les données par continent ou par pays
     data = pays
-        ? data.filter(element['Country'] === pays)
-        : data;
+        ? data.filter(element => element['Country'] === pays) // Filtrer par pays
+        : data.filter(element => Object.values(EUROPE_COUNTRIES).includes(element['Country']) || Object.values(AMERICA_COUNTRIES).includes(element['Country'])); // Filtrer par continent
 
     for (const element of data) {
         const workExp = parseInt(element['WorkExp']);
-        const salary = parseFloat(element['CompTotal']);
+        const salary = parseInt(element['CompTotal']);
         //
         if (!isNaN(workExp) && !isNaN(salary)) {
             uniqueWorkExp.add(workExp);
@@ -24,9 +44,9 @@ function getMeanSalaryByExp(data, continent, pays) {
             if (meanSalaries[workExp] === undefined) {
                 meanSalaries[workExp] = {totalSalaires: 0, count: 0};
             }
-            meanSalaries[workExp].totalSalaires += salary;
+            meanSalaries[workExp].totalSalaires += Math.trunc(salary);
             meanSalaries[workExp].count += 1;
-            console.log(`Travail Expérience : ${workExp}, Salaire : ${salary}`);
+            console.log(`Travail Expérience : ${workExp}, Salaire : ${Math.trunc(salary)}`);
         }
     }
 
@@ -35,19 +55,19 @@ function getMeanSalaryByExp(data, continent, pays) {
     Object.keys(meanSalaries).forEach(workExp => {
         //condition pour éviter la division par 0
         if (meanSalaries[workExp].count !== 0) {
-            meanSalaries[workExp] = meanSalaries[workExp].totalSalaires / meanSalaries[workExp].count;
+            meanSalaries[workExp] = Math.round(meanSalaries[workExp].totalSalaires / meanSalaries[workExp].count);
         } else {
             meanSalaries[workExp] = 0;
         }
     });
-    return meanSalaries;
+     return meanSalaries;
 }
 
 // Code à exécuter en cas de succès de la requête
 request.done(function (output) {
     let meanSalaries = getMeanSalaryByExp(output);
-     //meanSalaries = getMeanSalaryByExp(output,"" , "France");
-    console.log("Salaire moyen en France :", meanSalaries);
+     meanSalaries = getMeanSalaryByExp(output,"" , "");
+    console.log("Salaire moyen:",meanSalaries);
 
     // Mettez ici le reste de votre code qui utilise meanSalaries si nécessaire
 });
