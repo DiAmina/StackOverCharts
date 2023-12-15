de085b6d2d6f2156d3bea268322d0c91ae05ca16
Amdjad <amdjad9857@gmail.com>
2023-12-15 16:05:43 +0100

    Update Analyse ALL

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 // FICHIER DE CONFIGURATION DE L'APPLICATION WEB
-
 const config = {
-    app_name : "Analyse des données de Stack Overflow",
+    app_name : "StackOverChart",
     app_version : "1.0",
-    app_authors : "ANRIFOU AMDJAD & PREMI CARL",
+    app_authors : "ANRIFOU AMDJAD & MADI AMINA",
     app_date : "2023",
 
     // Specifier le chemin vers le fichier JSON contenant les jeux de données
+    // (Données non séparées en WE et NA !)
     url: "../data/survey_results.json"
 }
 
